c1694a0d2ff14025ca56dcb8aa96257521ba0b68
弓备 <gongbei@qihoodeMacBook-Pro-2.local>
2021-03-17 11:10:55 +0800

    面试手写代码

--- index.js
+++ index.js
@@ -81,5 +81,25 @@ function myNew(Func, ...args) {
   if (typeof res === "function" || (typeof res === "object" && res !== null)) {
     return res
   }
-  return instance
+  return instance;
+}
+/**
+ * bind
+ * @param {context} 
+ * @return {fn}
+ */
+Function.prototype.myBind = function (context = globalThis) {
+  const fn = this;
+  const args = Array.from(arguments).slice(1)
+  const newFunc = function () {
+    const newArgs = args.concat(...arguments);
+    if (this instanceof newFunc) {  // 通过 new 调用，绑定 this 为实例对象
+      fn.apply(this, newArgs);
+    } else {
+      // 通过普通函数形式调用，绑定 context
+      fn.apply(context, newArgs);
+    };
+  }
+  // 支持 new 调用方式newFunc.prototype = Object.create(fn.prototype)
+  return newFunc;
 }
